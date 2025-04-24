d08a8666641baaf2e8dc4fd17f4c4a51f3559d9c
José Paulo Oliveira Filho <josepaulooliveirafilho@gmail.com>
2025-04-24 06:47:37 -0300

    fix: User API controller index response ordering

--- laravel-api/app/Http/Controllers/UserController.php
+++ laravel-api/app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::select('id', 'name', 'email')->orderBy('id', 'desc')->get();
+        $users = User::select('id', 'name', 'email')->get();
 
         return response()->json($users);
     }
